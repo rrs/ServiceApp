2f8a06c976c3ea109d1fed360db64b7328bc165e
Robert Snipe <bobblezor@gmail.com>
2018-07-04 18:01:58 +0100

    added installer files

---
+++ src/Rrs.ServiceApp/content/MyServiceInstaller.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace $rootnamespace$
+{
+    [RunInstaller(true)]
+    public sealed class MyServiceInstaller : ServiceInstaller
+    {
+        public MyServiceInstaller()
+        {
+            Description = "My Service Description";
+            DisplayName = "My Service Display Name";
+            ServiceName = "MyServiceName";
+            StartType = ServiceStartMode.Automatic;
+        }
+    }
+}
---
+++ src/Rrs.ServiceApp/content/MyServiceInstallerProcess.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ServiceProcess;
+
+namespace $rootnamespace$
+{
+    [RunInstaller(true)]
+    public sealed class MyServiceInstallerProcess : ServiceProcessInstaller
+    {
+        public MyServiceInstallerProcess()
+        {
+            Account = ServiceAccount.LocalSystem;
+        }
+    }
+}
