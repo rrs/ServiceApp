052f8fcfc7d9a8483cd2ba15af0f3e73828316d4
Robert Snipe <robertjsnipe@gmail.com>
2019-09-23 14:59:38 +0100

    Added description / display name support

--- src/Rrs.ServiceApp/ProgramHelper.cs
+++ src/Rrs.ServiceApp/ProgramHelper.cs
@@ -8,7 +8,7 @@ namespace Rrs.ServiceApp
 {
     public static class ProgramHelper
     {
-        public static int Run(ServiceBase service, Func<IWpfApp> appFunc, string[] args)
+        public static int Run(ServiceBase service, Func<IWpfApp> appFunc, string[] args, string serviceDescription = null)
         {
             try
             {
